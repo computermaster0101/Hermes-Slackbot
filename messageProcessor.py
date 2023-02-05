9bc2dcaa4fee962ea575cec8e04eb9a49344c2d0
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-02-05 14:46:06 -0700

    Update messageProcessor.py
    updated logic

--- messageProcessor.py
+++ messageProcessor.py
@@ -1,9 +1,21 @@
-class MessageProcessor:
-    def __init__(self, save_file, message, rules_directory=''):
-        self.matched = False
-        self.saveFile = save_file  # identify the log location for the received message
-        self.message = message
-        self.rulesDirectory = rules_directory
-        self.ruleDictionary = []
+import re
+import subprocess
 
+class message_processor:
+    def __init__(self, rule_set):
+        self.rule_set = rule_set
 
+    def process_message(self, message):
+        for rule in self.rule_set.rules.values():
+            if not rule.active:
+                continue
+            for pattern in rule.patterns:
+                match = re.search(pattern, message.text)
+                if match:
+                    if not rule.passMessage:
+                        return
+                    for action in rule.actions:
+                        try:
+                            subprocess.run(action, cwd=rule.runningDirectory, shell=True, check=True)
+                        except subprocess.CalledProcessError as e:
+                            print(f"Error running action {action}: {e}")
