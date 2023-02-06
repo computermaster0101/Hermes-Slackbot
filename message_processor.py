a6a3a3dfb066f0a5bff7de99adac3ec66eed8832
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-02-05 18:48:37 -0700

    Update message_processor.py
    Logic needs to be corrected when pattern is matched

--- message_processor.py
+++ message_processor.py
@@ -7,17 +7,26 @@ class MessageProcessor:
         self.rule_set = rule_set
 
     def process_message(self, message, rules):
-        self.rule_set=rules
-        for rule in self.rule_set.rules.values():
-            if not rule.active:
-                continue
-            for pattern in rule.patterns:
-                match = re.search(pattern, message.text)
-                if match:
-                    if not rule.passMessage:
-                        return
-                    for action in rule.actions:
-                        try:
-                            subprocess.run(action, cwd=rule.runningDirectory, shell=True, check=True)
-                        except subprocess.CalledProcessError as e:
-                            print(f"Error running action {action}: {e}")
+        try:
+            self.rule_set=rules
+            for rule in self.rule_set.rules.values():
+                print(f"Processing rule: {rule}")
+                if not rule.active:
+                    print(f"Rule {rule} is inactive. Skipping.")
+                    continue
+                for pattern in rule.patterns:
+                    print(f"Matching message '{message.text}' against pattern '{pattern}'")
+                    match = re.search(pattern, message.text)
+                    if match:
+                        print(f"Message '{message.text}' matches pattern '{pattern}'")
+                        if not rule.passMessage:
+                            print(f"Rule {rule} is set to not pass message. Returning.")
+                            return
+                        for action in rule.actions:
+                            print(f"Running action '{action}' in directory '{rule.runningDirectory}'")
+                            try:
+                                subprocess.run(action, cwd=rule.runningDirectory, shell=True, check=True)
+                            except subprocess.CalledProcessError as e:
+                                print(f"Error running action {action}: {e}")
+        except Exception as e:
+            print(f"An error occurred while processing message: {e}")
