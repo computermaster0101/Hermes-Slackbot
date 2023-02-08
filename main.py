60f6af3c71b35caa8090d920fa2c386329c22752
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-02-08 13:10:59 -0800

    updates to main
    added process_message and output handling to main

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import json
 import threading
 import time
 
+env = "development"
+
 
 class Main:
     def __init__(self, config_file):
@@ -27,33 +29,43 @@ class Main:
             self.audio_listener = AudioListener()
             self.rules = RuleSet(self.rulesDirectory)
             self.message_processor = MessageProcessor(self.rules)
-            self.file_system_thread = threading.Thread(target=self.process_file_system_message)
-            self.audio_thread = threading.Thread(target=self.process_audio_message)
+            self.file_system_thread = threading.Thread(target=self.wait_for_file_system_message)
+            self.audio_thread = threading.Thread(target=self.wait_for_audio_message)
         except FileNotFoundError as e:
             print(f"Error loading config from {self.configFile}: {e}")
         except Exception as e:
             print(f"Error loading config: {e}")
 
-    def process_file_system_message(self):
+    def wait_for_file_system_message(self):
         print(f"Listening for message file {self.messageFile}")
         while True:
-            message = Message(message_file=self.messageFile)
-            if message.timestamp:
-                ## todo: reload rules incase rules have changed
-                self.message_processor.process_message(message, self.rules)
+            if os.path.exists(self.messageFile):
+                message = Message(message_file=self.messageFile)
+                self.process_message(message)
             time.sleep(0.5)
 
-    def process_audio_message(self):
+    def wait_for_audio_message(self):
+        print(f"Listening for keyword file {self.keyword}")
         while True:
             audio_event = self.audio_listener.get_message_from_audio()
             if audio_event:
                 message = Message(message_object=audio_event)
-                ## todo: reload rules incase rules have changed
-                self.message_processor.process_message(message, self.rules)
+                self.process_message(message)
             time.sleep(0.5)
 
+    def process_message(self, message):
+        self.rules = RuleSet(self.rulesDirectory)
+        match, output = self.message_processor.process_message(message, self.rules)
+        history_file = os.path.join(self.historyDirectory, f"{time.strftime('%Y%m%d-%H%M%S')}_{self.systemName}.txt")
+        with open(history_file, "w") as f:
+            f.write(f"{message}\n{output}")
+        if env == "production":
+            os.remove(self.messageFile)
+        else:
+            os.rename(self.messageFile, self.messageFile + ".bak")
+        print(*map(print, output))
+
     def run(self):
-        # print("commented out for testing")
         self.file_system_thread.start()
         self.audio_thread.start()
         self.file_system_thread.join()
--- message_processor.py
+++ message_processor.py
@@ -10,15 +10,16 @@ class MessageProcessor:
 
     def process_message(self, message, rules):
         try:
+            output = []
             match = False
             self.rule_set = rules
             for rule in self.rule_set.rules.values():
-                print(f"Processing rule: {rule}")
+                output.append(f"Processing rule: {rule}")
                 for pattern in rule.patterns:
-                    print(f"Matching message '{message.text}' against pattern '{pattern}'")
+                    output.append(f"Matching message '{message.text}' against pattern '{pattern}'")
                     if re.search(pattern, message.text):
                         match = True
-                        print(f"Message '{message.text}' matches pattern '{pattern}'")
+                        output.append(f"Message '{message.text}' matches pattern '{pattern}'")
                         if rule.active:
                             # return_path = os.getcwd()  # Store the current working directory in return_path
                             # if os.path.exists(rule.runningDirectory): os.chdir(rule.runningDirectory)  # if the running directory exists move to it
@@ -27,17 +28,19 @@ class MessageProcessor:
                                     cmd = f"{action} \"{message.text}\""
                                 else:
                                     cmd = action
-                                print(f"Running action '{action}' in directory '{rule.runningDirectory}'")
+                                output.append(f"Running action '{action}' in directory '{rule.runningDirectory}'")
                                 try:
                                     subprocess.run(action, cwd=rule.runningDirectory, shell=True, check=True)
                                     # subprocess.Popen(shlex.split(cmd))
                                 except subprocess.CalledProcessError as e:
-                                    print(f"Error running action {action}: {e}")
+                                    output.append(f"Error running action {action}: {e}")
                             # os.chdir(return_path)
                         else:
-                            print(f"Rule {rule} is inactive. Skipping.")
+                            output.append(f"Rule {rule} is inactive. Skipping.")
                             continue
             if not match:  # if the pattern/message does not match log it
-                print(f"Undefined Pattern: {message.text}")
+                output.append(f"Undefined Pattern: {message.text}")
+            return match, output
+
         except Exception as e:
             print(f"An error occurred while processing message: {e}")
