00c10a6eabca66f77fc7f29af0662b2aa657286e
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-03-02 17:13:41 -0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,9 @@ import urllib.parse
 import urllib.request
 from urllib.parse import parse_qs
 from datetime import datetime
-from upload_file import FileUploader
 
-api_token="Vsu9TRxpQ4cMIwvs409GJzzjcLPHNtduu5SWMChNregJ1UdoSNnwThsUtSvpxinV"
-dropbox_access_token="sl.BZ2KV65_29wX3hTieM1s404IyYiwBpnWhJsfXBVLTqb1GYfiYJZaSwbRNY6A5vq5WGzR0mANgUwjVabYjDOaF70n0mJK5UxxPfhZchmAk1jqFx5Kshg-JAV0S872KotYW2S3PqI"
+api_token = ""
+dropbox_access_token = ""
 
 
 def lambda_handler(event, context):
@@ -27,17 +26,25 @@ def lambda_handler(event, context):
         api_key = event['queryStringParameters']['apikey']
         if api_key == api_token:
             print('The api token has been validated!')
-            method=""
+            respond = False
             if event['isBase64Encoded']:
                 body = base64.b64decode(event['body']).decode('utf-8')
                 parsed_body = parse_qs(body)
                 text = parsed_body['text'][0]
                 print(f'Received / command: {text}')
-                method='slash'
+            elif json.loads(event['body'])['event']['text']:
+                method = 'at'
+                event_body = json.loads(event['body'])
+                text = event_body['event']['text']
+                team = event_body['event']['team']
+                channel = event_body['event']['channel']
+                respond = True
             else:
-                text = json.loads(event['body'])['event']['blocks'][0]['elements'][0]['elements'][1]['text'].strip()
-                print(f'Received @ command: {text}')
-                method='at'
+                event_body = json.loads(event['body'])
+                text = event_body['event']['blocks'][0]['elements'][0]['elements'][1]['text'].strip()
+                team = event_body['event']['team']
+                channel = event_body['event']['channel']
+                respond = True
 
             match = re.match(r'system\s*(\d+)\s*(.*)', text)
             if match:
@@ -53,26 +60,32 @@ def lambda_handler(event, context):
                 try:
                     upload_file = FileUploader(dropbox_access_token=dropbox_access_token)
                     upload_file.dropbox(file_name, json.dumps(message_object))
-                    output=f'Hello from Hermes!\nYour message is being promptly delivered!\n{json.dumps(message_object)}'
-                    select_message_return_method(method,output)
-                    return {
-                        'statusCode': 202,
-                        'body': f'{output}'
-                    }
+                    output = f'Hello from Hermes!\nYour message is being promptly delivered!\n{json.dumps(message_object)}'
+                    if respond:
+                        send_message_to_slack(output, channel)
+                    else:
+                        return {
+                            'statusCode': 202,
+                            'body': f'{output}'
+                        }
                 except Exception as e:
-                    output='An internal server error occurred:\n' + str(e)
-                    select_message_return_method(method,output)
+                    output = 'An internal server error occurred:\n' + str(e)
+                    if respond:
+                        send_message_to_slack(output, channel)
+                    else:
+                        return {
+                            'statusCode': 202,
+                            'body': f'{output}'
+                        }
+            else:
+                output = f'The pattern did not match!\ninvalid command:\n{text}'
+                if respond:
+                    send_message_to_slack(output, channel)
+                else:
                     return {
                         'statusCode': 202,
                         'body': f'{output}'
                     }
-            else:
-                output=f'The pattern did not match!\ninvalid command:\n{text}'
-                select_message_return_method(method,output)
-                return {
-                    'statusCode': 202,
-                    'body': f'{output}'
-                }
         else:
             print('The api token is invalid!')
             return {
@@ -87,33 +100,45 @@ def lambda_handler(event, context):
             'body': 'internal server error'
         }
 
-def select_message_return_method(method, message):
-    print(message)
-    if method == "at":
-        return send_message_to_slack(message)
 
-def return_message_to_slack(message):
-    return {
-        'statusCode': 202,
-        'body': 'static string for testing'
-    }
-
-def send_message_to_slack(message):
-    slack_url="https://slack.com/api/chat.postMessage"
-    channel_id="C04S2PX6U9K"
-    token="xoxb-1620285266083-4876057954115-NQ512QTznSI7fz02lKWM32EB"
+def send_message_to_slack(message, channel):
+    slack_url = "https://slack.com/api/chat.postMessage"
+    token = ""
 
     data = urllib.parse.urlencode(
         (
             ("token", token),
-            ("channel", channel_id),
+            ("channel", channel),
             ("text", message)
         )
     )
     data = data.encode("ascii")
 
     request = urllib.request.Request(slack_url, data=data, method="POST")
-    request.add_header( "Content-Type", "application/x-www-form-urlencoded")
+    request.add_header("Content-Type", "application/x-www-form-urlencoded")
     x = urllib.request.urlopen(request).read()
     print(x)
-    return { 'statusCode': 202 }
+    return {'statusCode': 202}
+
+
+import os
+import dropbox
+
+
+class FileUploader:
+    def __init__(self, dropbox_access_token):
+        self.dbx = dropbox.Dropbox(dropbox_access_token)
+
+    def dropbox(self, file_name, content):
+        tmp_file_path = os.path.join('/tmp', file_name)
+        with open(tmp_file_path, 'w') as f:
+            f.write(content)
+        with open(tmp_file_path, 'rb') as f:
+            try:
+                # self.dbx.files_upload(f.read(), '/Apps/Commands/' + file_name, mode=dropbox.files.WriteMode.overwrite)
+                self.dbx.files_upload(f.read(), '/Apps/Commands/' + file_name)
+                print(f"{file_name} uploaded successfully to Dropbox.")
+            except dropbox.exceptions.ApiError as e:
+                error_message = f"Failed to upload {file_name} to Dropbox\nDetails:\n{e}"
+                print(error_message)
+                raise Exception(error_message)
