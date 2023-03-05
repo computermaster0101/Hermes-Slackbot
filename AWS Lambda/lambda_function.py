305966f40cfa29813e040d197b5ad550d4a35db5
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-03-04 17:44:08 -0800

    Update lambda_function.py
    added better access control

--- AWS Lambda/lambda_function.py
+++ AWS Lambda/lambda_function.py
@@ -8,18 +8,24 @@ from send_message import MessageSender
 from upload_file import FileUploader
 
 # passed in as a query parameter for validating access to the funtion
-api_token = ""
+# passed in as a query parameter for validating access to the funtion.
+# different sources can have different tokens to help identify origin.
+tokens = {
+    "at_api_key": None,
+    "slash_api_key": None,
+    "dialogflow_api_key": None
+}
 
 # required for uploading to nextcloud
-nextcloud_username = ""
-nextcloud_access_token = ""
-nextcloud_url = ""
+nextcloud_username = None
+nextcloud_access_token = None
+nextcloud_url = None
 
 # dropbox_access_token expires every 4 hours
-dropbox_access_token = ""
+dropbox_access_token = None
 
 # required for sending messages as a slackbot
-slack_token = ""
+slack_token = None
 
 
 def lambda_handler(event, context):
@@ -36,23 +42,28 @@ def lambda_handler(event, context):
     """
     try:
         api_key = event['queryStringParameters']['apikey']
-        if api_key == api_token:
+        if api_key in tokens.values():
             print('The api token has been validated!')
             respond = False
-            if event['isBase64Encoded']:
+            if api_key == tokens['slash_api_key']:
                 body = base64.b64decode(event['body']).decode('utf-8')
                 parsed_body = parse_qs(body)
                 text = parsed_body['text'][0]
                 print(f'Received / command: {text}')
-            else:
+            elif api_key == tokens['at_api_key']:
                 event_body = json.loads(event['body'])
                 text = event_body['event']['blocks'][0]['elements'][0]['elements'][1]['text'].strip()
-                team = event_body['event']['team']
                 channel = event_body['event']['channel']
                 respond = True
                 if not event.get('headers').get('x-slack-retry-num') is None:
                     print("returning status code 202 to slackbot retry attempt")
                     return {'statusCode': 200}
+            else:
+                print("Not Yet Implimented")
+                return {
+                    'statusCode': 200,
+                    'body': "Not Yet Implimented"
+                }
 
             send_message = MessageSender(slack_token)
 
