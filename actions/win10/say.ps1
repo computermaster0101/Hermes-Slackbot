d3406c33fc48d2a83c113eef435d06276bcf829f
computermaster0101 <computermaster0101@users.noreply.github.com>
2023-08-22 15:56:59 -0700

    Update say.ps1
    fixed say

--- actions/win10/say.ps1
+++ actions/win10/say.ps1
@@ -1,14 +1,5 @@
 # Extract the string after "say"
 $argument = $args -replace '^say\s*', ''
-
-# Capitalize the first letter of the extracted argument
-$capitalizedArgument = $argument -replace '^(.)(.*)$', '$1'.ToUpper() + '$2'
-
-# Check if the extracted argument is empty
-if ($capitalizedArgument -ne '') {
-    # Display the extracted argument in a message box
-    powershell.exe -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('$capitalizedArgument','Hermes Said','OK','Information')"
-} else {
-    # Display an error message if no argument is provided after "say"
-    powershell.exe -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('No message provided.','Hermes Said','OK','Error')"
-}
+$capitalizedString = "$($argument.Substring(0,1).ToUpper())$($argument.Substring(1))"
+$capitalizedStringWithExclamation = $capitalizedString + "!"
+powershell.exe -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('$capitalizedStringWithExclamation','Hermes Said','OK','Information')"
