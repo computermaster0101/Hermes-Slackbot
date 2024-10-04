9b1c38bb7c45e782da63550cbbfa832c3c03e188
computermaster0101 <computermaster0101@users.noreply.github.com>
2024-10-03 20:21:03 -0700

    Repair nextcloud upload by updating package to nc_py_api

--- AWS Lambda/upload_file.py
+++ AWS Lambda/upload_file.py
@@ -1,21 +1,23 @@
 import os
 import dropbox
-from nextcloud import Client
-import requests
+from io import BytesIO
+from PIL import Image  # Make sure you have Pillow installed
+import nc_py_api
 
 
 class FileUploader:
 
     def __init__(self, dbox=None, nextcloud=None):
         print("FileUploader.__init__")
-        self.is_local = False,
+        self.is_local = False
         if dbox:
             self.dbx = dropbox.Dropbox(dbox['token'])
         if nextcloud:
-            session = requests.Session()
-            session.verify = False  # for self-signed certificates
-            self.nc = Client(url=nextcloud['url'])
-            self.nc.login(nextcloud['username'], nextcloud['token'])
+            self.nc = nc_py_api.Nextcloud(
+                nextcloud_url=nextcloud['url'],
+                nc_auth_user=nextcloud['username'],
+                nc_auth_pass=nextcloud['token']
+            )
 
     def upload(self, file_name, content):
         print("FileUploader.upload")
@@ -38,9 +40,12 @@ class FileUploader:
                     error_message = f"Failed to upload {file_name} to Dropbox\nDetails:\n{e}"
                     print(error_message)
                     raise Exception(error_message)
+
         if hasattr(self, 'nc'):
             try:
-                self.nc.put_file(remote_path, tmp_file_path)
+                with open(tmp_file_path, 'rb') as f:  # Open the file in binary mode
+                    buf = BytesIO(f.read())  # Create a BytesIO stream from the file content
+                    self.nc.files.upload_stream(remote_path, buf)  # Use the upload_stream method
                 print(f"{file_name} uploaded successfully to Nextcloud.")
             except Exception as e:
                 error_message = f"Failed to upload {file_name} to Nextcloud\nDetails:\n{e}"
